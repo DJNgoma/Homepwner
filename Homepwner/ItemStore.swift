7a7038d2e9c3b2aed88bd10440b8b3139f0a0f6d
Daliso Joshua Ngoma <daliso@djngoma.com>
2016-03-22 18:28:45 +0800

    Done with Chapter 9

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,29 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Daliso Ngoma on 21/03/2016.
+//  Copyright © 2016 Daliso Ngoma. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    
+    var allItems = [Item]()
+    
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+}
