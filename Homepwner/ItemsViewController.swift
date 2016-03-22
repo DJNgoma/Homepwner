657ed004a17a9554d93bff372e084bd6b696c27f
Daliso Joshua Ngoma <daliso@djngoma.com>
2016-03-22 20:22:16 +0800

    Bronze Challenge Complete

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -11,6 +11,21 @@ import UIKit
 class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
+    var itemStoreGreaterThan50: [Item] {
+        var items = [Item]()
+        for item: Item in itemStore.allItems where item.valueInDollars > 50 {
+            items.append(item)
+        }
+        return items
+    }
+    
+    var itemStoreLessThan50: [Item] {
+        var items = [Item]()
+        for item: Item in itemStore.allItems where item.valueInDollars <= 50 {
+            items.append(item)
+        }
+        return items
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +40,29 @@ class ItemsViewController: UITableViewController {
     
     // MARK: UITableViewDataSource methods
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("The count for the amount of sections is: \(itemStore.allItems.count)")
-        return itemStore.allItems.count
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
     }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return itemStoreGreaterThan50.count
+        }
+        else {
+            return itemStoreLessThan50.count
+        }
+    }
+    
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return ">50"
+        }
+        else {
+            return "<=50"
+        }
+    }
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
@@ -38,7 +72,15 @@ class ItemsViewController: UITableViewController {
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will appear in on the tableview
-        let item = itemStore.allItems[indexPath.row]
+        
+        var item: Item
+        
+        if indexPath.section == 0 {
+            item = itemStoreGreaterThan50[indexPath.row]
+        }
+        else {
+            item = itemStoreLessThan50[indexPath.row]
+        }
         
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
